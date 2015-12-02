ce6ca84749404b4bdef0cdb3ad8f718d43bb7b91
yomotsu <oyamada@pxgrid.com>
2015-12-03 02:46:37 +0900

    build

--- build/assets2/js/codegrid-ui.js
+++ build/assets2/js/codegrid-ui.js
@@ -170,6 +170,24 @@ CG2.vent = new EventDispatcher();
 
 } )();
 
+window.addEventListener( 'DOMContentLoaded', function () {
+
+  'use strict';
+
+  var modifier = 'CG2-pageHeader__userMenu--show';
+  var $head = document.querySelector( '.CG2-pageHeader__userHeader' );
+  var $nav  = document.querySelector( '.CG2-pageHeader__userMenu' );
+
+  var navToggle = function () {
+
+    $nav.classList.toggle( modifier );
+
+  }
+
+  $head.addEventListener( 'click', navToggle );
+
+} );
+
 /**
  * @author yomotsu / http://yomotsu.net
  * repository: https://github.com/yomotsu/PXG-drawer
