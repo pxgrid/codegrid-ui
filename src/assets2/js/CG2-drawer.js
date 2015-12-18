c994290337781537e9a77955f81bbe1c2f944401
yomotsu <oyamada@pxgrid.com>
2015-12-18 20:53:21 +0900

    ドロワーがスワイプで閉じるように #33

--- src/assets2/js/CG2-drawer.js
+++ src/assets2/js/CG2-drawer.js
@@ -7,10 +7,37 @@ window.addEventListener( 'DOMContentLoaded', function () {
 
   'use strict';
 
-  var i = 0;
-  var modifier = 'js-drawer--show';
-  var html = document.documentElement;
-  var body = document.body;
+  var DISTANCE_THRESHOLD = 5;
+  var ANGLE_THREHOLD = 60;
+  var WILL_CLOSE_X = 30;
+
+  var ua = ( function () {
+    return {
+      pointer: window.navigator.pointerEnabled,
+      MSPoniter: window.navigator.msPointerEnabled,
+      touch: typeof document.ontouchstart !== 'undefined'
+    }
+  } )();
+  var _pointerstart = ua.pointer   ? 'pointerdown' :
+                      ua.MSPoniter ? 'MSPointerDown' :
+                      ua.touch     ? 'touchstart' :
+                      'mousedown';
+  var _pointermove  = ua.pointer   ? 'pointermove' :
+                      ua.MSPoniter ? 'MSPointerMove' :
+                      ua.touch     ? 'touchmove' :
+                      'mousemove';
+  var _pointerend   = ua.pointer   ? 'pointerup' :
+                      ua.MSPoniter ? 'MSPointerUp' :
+                      ua.touch     ? 'touchend' :
+                      'mouseup';
+
+  var modifier = {
+    show    : 'js-drawer--show',
+    dragging: 'js-drawer--dragging'
+  };
+  var $win  = window;
+  var $html = document.documentElement;
+  var $body = document.body;
   var scrollTop;
   var scrollbarWidth = ( function () {
 
@@ -21,7 +48,7 @@ window.addEventListener( 'DOMContentLoaded', function () {
     outer.style.width = '100px';
     outer.style.msOverflowStyle = 'scrollbar';
 
-    document.body.appendChild( outer );
+    $body.appendChild( outer );
 
     var widthNoScroll = outer.offsetWidth;
     // force scrollbars
@@ -41,56 +68,68 @@ window.addEventListener( 'DOMContentLoaded', function () {
 
   } )();
 
-  var isHidden = true;
-  var openEl, closeEl, toggleEl;
-  var openEls   = document.querySelectorAll( '[data-drawer-show]' );
-  var closeEls  = document.querySelectorAll( '[data-drawer-hide]' );
-  var toggleEls = document.querySelectorAll( '[data-drawer-toggle]' );
+  var isDragReady = false;
+  var isDragging  = false;
+  var isHidden    = true;
 
-  for ( i = 0, openEl; openEl = openEls[ i ]; i = ( i + 1 )|0 ) {
+  var _pointerId = null;
+  var _pointerId = null;
+  var dragStartCoord = { x: 0|0, y: 0|0 };
+  var $openEl, $closeEl, $toggleEl;
+  var $panel     = document.querySelector( '.CG2-drawer__panel' );
+  var $openEls   = document.querySelectorAll( '[data-drawer-show]' );
+  var $closeEls  = document.querySelectorAll( '[data-drawer-hide]' );
+  var $toggleEls = document.querySelectorAll( '[data-drawer-toggle]' );
 
-    openEl.addEventListener( 'click', navOpen );
+  Array.prototype.forEach.call( $openEls, function( $openEl ) {
 
-  }
+    $openEl.addEventListener( 'click', navOpen );
 
-  for ( i = 0, closeEl; closeEl = closeEls[ i ]; i = ( i + 1 )|0 ) {
+  } );
 
-    closeEl.addEventListener( 'click', navClose );
+  Array.prototype.forEach.call( $closeEls, function( $closeEl ) {
 
-  }
+    $closeEl.addEventListener( 'click', navClose );
 
-  for ( i = 0, toggleEl; toggleEl = toggleEls[ i ]; i = ( i + 1 )|0 ) {
+  } );
 
-    toggleEl.addEventListener( 'click', navToggle );
+  Array.prototype.forEach.call( $toggleEls, function( $toggleEl ) {
 
-  }
+    $toggleEl.addEventListener( 'click', navToggle );
+
+  } );
+
+  $panel.addEventListener( _pointerstart, dragstart );
+
+  //
 
   function navOpen () {
 
     if ( !isHidden ) { return; }
 
     isHidden = false;
-    scrollTop = html.scrollTop || body.scrollTop;
-    html.className += ' ' + modifier;
-    html.style.paddingRight  = scrollbarWidth + 'px';
-    body.style.marginTop     = -scrollTop     + 'px';
-    body.style.paddingBottom =  scrollTop     + 'px';
+    scrollTop = $html.scrollTop || $body.scrollTop;
+    $html.className += ' ' + modifier.show;
+    $html.style.paddingRight  = scrollbarWidth + 'px';
+    $body.style.marginTop     = -scrollTop     + 'px';
+    $body.style.paddingBottom =  scrollTop     + 'px';
+    $panel.style.marginLeft   = 0;
 
-  };
+  }
 
   function navClose () {
 
     if ( isHidden ) { return; }
 
-    var re = new RegExp( ' ' + modifier );
+    var re = new RegExp( ' ' + modifier.show );
     isHidden = true;
-    html.className = html.className.replace( re, '' );
-    html.style.paddingRight  = 0;
-    body.style.marginTop     = 0;
-    body.style.paddingBottom = 0;
+    $html.className = $html.className.replace( re, '' );
+    $html.style.paddingRight  = 0;
+    $body.style.marginTop     = 0;
+    $body.style.paddingBottom = 0;
     window.scrollTo( 0, scrollTop );
 
-  };
+  }
 
   function navToggle () {
 
@@ -104,6 +143,234 @@ window.addEventListener( 'DOMContentLoaded', function () {
 
     }
 
+  }
+
+  function dragstart ( event ) {
+
+    var eventCoord;
+
+    if ( !ua.touch ) {
+
+      if ( /A|BUTTON|INPUT|TEXTAREA|SELECT/.test( event.target.nodeName ) ) {
+
+        return;
+
+      }
+
+      // prevent text/element selection with cursor drag
+      event.stopPropagation();
+      event.preventDefault();
+      event.cancelBubble = true;
+      event.returnValue = false;
+
+    }
+
+    if ( event.pointerId ) {
+
+      _pointerId = event.pointerId;
+
+    } else if ( event.changedTouches ) {
+
+      _pointerId = event.changedTouches[ event.changedTouches.length - 1 ].identifier;
+
+    }
+
+    eventCoord = getEventCoord( event );
+    dragStartCoord.x = eventCoord.x;
+    dragStartCoord.y = eventCoord.y;
+
+    isDragReady = false;
+    isDragging  = false;
+
+    $panel.addEventListener( _pointermove, dragmove );
+    $panel.addEventListener( _pointerend, dragend );
+
+  }
+
+  function dragmove ( event ) {
+
+    // var x;
+    var eventCoord;
+
+    if ( !ua.touch ) {
+
+      // prevent text/element selection with cursor drag
+      event.stopPropagation();
+      event.preventDefault();
+      event.cancelBubble = true;
+      event.returnValue = false;
+
+    }
+
+    eventCoord = getEventCoord( event );
+    var x = Math.max( eventCoord.x - dragStartCoord.x, 0 );
+
+    if ( !isDragReady ) {
+
+      var triangle = getTriangleSide(
+        dragStartCoord.x,
+        dragStartCoord.y,
+        eventCoord.x,
+        eventCoord.y
+        // eventCoord.x,
+        // eventCoord.y
+      );
+
+      if ( triangle.z > DISTANCE_THRESHOLD ) {
+
+        if ( getAngle( triangle ) > ANGLE_THREHOLD ) {
+
+          isDragReady = true;
+
+        } else {
+
+          dragend( event );
+          return;
+
+        }
+
+      } else {
+
+        return;
+
+      }
+
+    }
+
+    event.stopPropagation();
+    event.preventDefault();
+    event.cancelBubble = true;
+    event.returnValue = false;
+
+    if ( !isDragging ) {
+
+      $html.className += ' ' + modifier.dragging;
+      isDragging = true;
+
+    }
+
+    $panel.style.marginLeft = x + 'px';
+
+  }
+
+  function dragend ( event ) {
+
+    var x, re, i, l;
+
+    if ( event.pointerId && pointerId !== event.pointerId ) {
+
+      return;
+
+    } else if ( event.changedTouches ) {
+
+      for ( i = 0, l = event.changedTouches.length; i < l; i ++ ) {
+
+        if ( _pointerId === event.changedTouches[ i ].identifier ) {
+
+          break;
+
+        }
+
+        if ( i === event.changedTouches.length ) {
+
+          return;
+
+        }
+
+      }
+
+    }
+
+    x = Math.max( getEventCoord( event ).x - dragStartCoord.x, 0 );
+
+    if ( WILL_CLOSE_X < x ) {
+
+      navClose();
+
+    } else {
+
+      $panel.style.marginLeft = 0;
+
+    }
+
+    re = new RegExp( ' ' + modifier.dragging );
+    $html.className = $html.className.replace( re, '' );
+
+    $panel.removeEventListener( _pointermove, dragmove );
+    $panel.removeEventListener( _pointerend, dragend );
+
+    isDragReady = false;
+    isDragging  = false;
+
+  }
+
+  function getEventCoord ( event ) {
+
+    var x, y, i, l, _event = null;
+
+    if ( event.pointerId ) {
+
+      if ( _pointerId === event.pointerId ) {
+
+        _event = event;
+
+      }
+
+    } else if ( event.changedTouches ) {
+
+      for ( i = 0|0, l = event.changedTouches.length; i < l; i = ( i + 1 )|0 ) {
+
+        if ( _pointerId === event.changedTouches[ i ].identifier ) {
+
+          _event = event.changedTouches[ i ];
+
+        }
+
+      }
+
+    } else {
+
+      _event = event;
+
+    }
+
+    if ( _event === null ) {
+
+      return false;
+
+    }
+
+    x = _event.clientX|0;
+    y = _event.clientY|0;
+
+    return { x: x, y: y };
+
   };
+
+  function getTriangleSide( x1, y1, x2, y2 ) {
+
+    var x = Math.abs( x1 - x2 );
+    var y = Math.abs( y1 - y2 );
+    var z = Math.sqrt( x * x + y * y );
+
+    return {
+      x: x,
+      y: y,
+      z: z
+    };
+
+  }
+
+  function getAngle( triangle ) {
+
+    var cos = triangle.y / triangle.z;
+    var radian = Math.acos( cos );
+
+    console.log( 180 / ( Math.PI / radian ) );
+
+    return 180 / ( Math.PI / radian );
+
+  }
+
 
 } );
