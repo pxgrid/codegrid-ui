22f4666ebd783d8305381e1cf62ccae995ed8256
Masataka Yakura <masataka.yakura@gmail.com>
2017-04-25 01:09:22 +0900

    selectのプレースホルダ用JavaScript：関数をくるんだ
    フォーム用という意図なので他のフォーム関連スタイルも書けるように関数でくるんだ。
    あわせてselectがなかった場合死んでたバグを修正＆ついでにearly return実施。

--- src/assets2/js/CG2-forms.js
+++ src/assets2/js/CG2-forms.js
@@ -4,36 +4,42 @@ window.addEventListener('DOMContentLoaded', function () {
   //-
   //- ドロップダウンボックスのプレースホルダのスタイル
   //-
+  function setupSelectPlaceholder() {
+    // プレースホルダのクラス名
+    var CLASSNAME_SELECT_PLACEHOLDER = 'CG2-form__placeholder_selected'
 
-  // プレースホルダのクラス名
-  var CLASSNAME_SELECT_PLACEHOLDER = 'CG2-form__placeholder_selected'
+    var selects = document.querySelectorAll('.CG2-form__table select')
+    if (selects.length === 0) { return }
 
-  // プレースホルダを持った<select>のみ抽出
-  // <select>のプレースホルダは、最初の<option>のうちvalueが空なもの
-  // ref. https://html.spec.whatwg.org/multipage/forms.html#placeholder-label-option
-  var selects = document.querySelectorAll('.CG2-form__table select')
-  var selectsWithPlaceholder = Array.prototype.filter.call(selects, function (select) {
-    return select.querySelector('option[value=""]:first-child') !== null
-  })
-
-  // 初期表示とイベントハンドリング
-  selectsWithPlaceholder.forEach(function (select) {
-    select.addEventListener('change', function (event) {
-      renderSelectPlaceholder(select)
+    // プレースホルダを持った<select>のみ抽出
+    // <select>のプレースホルダは、最初の<option>のうちvalueが空なもの
+    // ref. https://html.spec.whatwg.org/multipage/forms.html#placeholder-label-option
+    var selectsWithPlaceholder = Array.prototype.filter.call(selects, function (select) {
+      return select.querySelector('option[value=""]:first-child') !== null
     })
-    renderSelectPlaceholder(select)
-  })
+    if (selectsWithPlaceholder.length === 0) { return }
 
-  // <option>を拾ってプレースホルダか否かでスタイルを切り替える
-  function renderSelectPlaceholder (select) {
-    var optionPlaceholder = select.querySelector('option[value=""]:first-child')
+    // 初期表示とイベントハンドリング
+    selectsWithPlaceholder.forEach(function (select) {
+      select.addEventListener('change', function (event) {
+        renderPlaceholder(select)
+      })
+      renderPlaceholder(select)
+    })
 
-    if (optionPlaceholder.selected) {
-      select.classList.add(CLASSNAME_SELECT_PLACEHOLDER)
-    }
-    else {
-      select.classList.remove(CLASSNAME_SELECT_PLACEHOLDER)
+    // <option>を拾ってプレースホルダか否かでスタイルを切り替える
+    function renderPlaceholder(select) {
+      var optionPlaceholder = select.querySelector('option[value=""]:first-child')
+
+      if (optionPlaceholder.selected) {
+        select.classList.add(CLASSNAME_SELECT_PLACEHOLDER)
+      }
+      else {
+        select.classList.remove(CLASSNAME_SELECT_PLACEHOLDER)
+      }
     }
   }
+
+  setupSelectPlaceholder()
 
 })
