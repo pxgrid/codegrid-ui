e9cd7390810330703b19f061b3685647449c7872
Masataka Yakura <masataka.yakura@gmail.com>
2017-04-25 05:42:50 +0900

    selectのプレースホルダ用JavaScript：名前づけ見直し
    変数名や関数名が自分でもよくわからなかったので直した。
    selectのグループは使いまわすことに。

--- src/assets2/js/CG2-forms.js
+++ src/assets2/js/CG2-forms.js
@@ -6,36 +6,38 @@ window.addEventListener('DOMContentLoaded', function () {
   //-
   function setupSelectPlaceholder() {
     // プレースホルダのクラス名
-    var CLASSNAME_SELECT_PLACEHOLDER = 'CG2-form__placeholder_selected'
+    var CLASSNAME_PLACEHOLDER_SELECTED = 'CG2-form__placeholder_selected'
 
-    var selects = document.querySelectorAll('.CG2-form__table select')
-    if (selects.length === 0) { return }
+    // フォーム内の<select>を抽出
+    // セレクタ固定にもやっとするが、いまのとこかなり特定のやつ（フォーム）に依存しているので…
+    var selectList = document.querySelectorAll('.CG2-form__table select')
+    if (selectList.length === 0) { return }
 
     // プレースホルダを持った<select>のみ抽出
     // <select>のプレースホルダは、最初の<option>のうちvalueが空なもの
     // ref. https://html.spec.whatwg.org/multipage/forms.html#placeholder-label-option
-    var selectsWithPlaceholder = Array.prototype.filter.call(selects, function (select) {
+    selectList = Array.prototype.filter.call(selectList, function (select) {
       return select.querySelector('option[value=""]:first-child') !== null
     })
-    if (selectsWithPlaceholder.length === 0) { return }
+    if (selectList.length === 0) { return }
 
     // 初期表示とイベントハンドリング
-    selectsWithPlaceholder.forEach(function (select) {
+    selectList.forEach(function (select) {
       select.addEventListener('change', function (event) {
-        renderPlaceholder(select)
+        updateView(select)
       })
-      renderPlaceholder(select)
+      updateView(select)
     })
 
     // <option>を拾ってプレースホルダか否かでスタイルを切り替える
-    function renderPlaceholder(select) {
-      var optionPlaceholder = select.querySelector('option[value=""]:first-child')
+    function updateView(select) {
+      var placeholderElement = select.querySelector('option[value=""]:first-child')
 
-      if (optionPlaceholder.selected) {
-        select.classList.add(CLASSNAME_SELECT_PLACEHOLDER)
+      if (placeholderElement.selected) {
+        select.classList.add(CLASSNAME_PLACEHOLDER_SELECTED)
       }
       else {
-        select.classList.remove(CLASSNAME_SELECT_PLACEHOLDER)
+        select.classList.remove(CLASSNAME_PLACEHOLDER_SELECTED)
       }
     }
   }
