16cf5577428a019166d4c28ba82ce6a5ab522a91
Masataka Yakura <masataka.yakura@gmail.com>
2018-07-29 17:16:38 +0900

    ダミー画像をつくるスクリプトを導入
    画像ファイル名を`__dummy__[w]x[h]`にするとダミー画像のSVGを動的に生成してそこにつっこむようにする。

---
+++ src/aigis-template/aigis_assets/js/dummyimage.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function createPlaceholderSVG(width, height) {
+  return `
+<svg width='${width}' height='${height}'>
+  <rect x='0' y='0' width='100%' height='100%' fill='#ccc'/>
+  <text x='50%' y='50%' text-anchor='middle' dominant-baseline='middle' fill='#fff' font-size='20' font-weight='bold'>${width}×${height}</text>
+</svg>`.replace(/\n\s*/, '').trim();
+}
+
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  const dummyImgs = [...document.querySelectorAll(`img[src^=__dummy__]`)];
+  dummyImgs.forEach(img => {
+    const srcValue = img.getAttribute(`src`).trim();
+    const svgUrl = srcValue.replace(/__dummy__(\d+)x(\d+)/, (_, p1, p2) => {
+      const svgSnippet = createPlaceholderSVG(p1, p2);
+      return `data:image/svg+xml,${encodeURIComponent(svgSnippet)}`;
+    });
+    img.src = svgUrl;
+  });
+})
